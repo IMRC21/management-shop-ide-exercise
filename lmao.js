a7a3649bcdd2e8f81c31617502480f0e0525484a
IMRC21 <marianoradu@mail.com>
2018-04-18 10:39:33 +0200

    ASD

--- lmao.js
+++ lmao.js
@@ -12,36 +12,38 @@ function add(){
 	ct++;
 	var cancella = document.createElement("BUTTON");
 	var txt = document.createTextNode("Cancella");
-	cancella.setAttribute("onclick", "cancellaRiga("+document.this.id + ")");
+	cancella.setAttribute("class", ct);
+	cancella.setAttribute("onclick", "cancellaRiga(this.className)");
 	cancella.appendChild(txt);
-	
-	
+
+
 	prod = document.getElementById("prod");
 	qua = document.getElementById("qua");
 	imp = document.getElementById("imp");
-	
+
 	tr = document.createElement("TR");
-	tr.setAttribute("id", ""+ct);
+	tr.setAttribute("class", ct);
 	document.getElementById("myTable").appendChild(tr);
 
 	let td = document.createElement("TD");
 	td.innerHTML = prod.value;
 	tr.appendChild(td);
-	
+
 	let td1 = document.createElement("TD");
 	td1.innerHTML = qua.value;
 	tr.appendChild(td1);
-	
+
 	let td2 = document.createElement("TD");
 	td2.innerHTML = imp.value;
 	tr.appendChild(td2);
-	
+
 	let td3 = document.createElement("TD");
 	td3.appendChild(cancella);
 	tr.appendChild(td3);
-	
+
 }
 
 function cancellaRiga(riceve){
-	document.getElementById("" + riceve).remove();
+	console.log(riceve);
+	document.getElementsByClassName("" + riceve).remove();
 }
